a1899e2b33ab1c3ea0c56cc2cbf9c46d5ba114e2
NugiNug <72553682+NugiNugroho@users.noreply.github.com>
2021-04-26 10:05:17 +0700

    Add Login and Register

---
+++ app/src/main/java/com/haerul/foodsapp/view/login/LoginActivity.java
@@ -0,0 +1,12 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 21/04/26 9:53                                                -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2021. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.haerul.foodsapp.view.login;
+
+import android.app.Activity;
+
+public class LoginActivity extends Activity {
+}
---
+++ app/src/main/java/com/haerul/foodsapp/view/login/RegisterActivity.java
@@ -0,0 +1,12 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 21/04/26 10:02                                               -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2021. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.haerul.foodsapp.view.login;
+
+import android.app.Activity;
+
+public class RegisterActivity extends Activity {
+}
